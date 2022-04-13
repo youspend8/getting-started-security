c491b758d5cb724bcf148154a809760b55b1fbf8
chaehoon <youspend@gmail.com>
2022-04-13 16:21:41 +0900

    feat : CommentController, PostController Method 작성 // TODO : implementation domain service

--- src/main/java/io/started/security/controller/CommentController.java
+++ src/main/java/io/started/security/controller/CommentController.java
@@ -1,12 +1,39 @@
 package io.started.security.controller;
 
+import io.started.security.domain.common.dto.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(path = "/comment", produces = MediaType.APPLICATION_JSON_VALUE)
 public class CommentController {
+    @PostMapping
+    public ResponseEntity<ApiResponse<Void>> post(
+            @RequestParam("postId") long postId) {
+        return ResponseEntity
+                .created(URI.create(String.format("/post/%d", postId)))
+                .body(ApiResponse.emptyOf(HttpStatus.CREATED));
+    }
+
+    @DeleteMapping(path = "/{commentId}")
+    public ResponseEntity<ApiResponse<Void>> delete(
+            @PathVariable("commentId") long commentId) {
+        return ResponseEntity
+                .noContent().build();
+    }
+
+    @PutMapping("/{commentId}")
+    public ResponseEntity<ApiResponse<Void>> put(
+            @RequestParam("postId") long postId,
+            @PathVariable("commentId") long commentId) {
+        return ResponseEntity
+                .created(URI.create(String.format("/post/%d", postId)))
+                .body(ApiResponse.emptyOf(HttpStatus.CREATED));
+    }
 }
--- src/main/java/io/started/security/controller/PostController.java
+++ src/main/java/io/started/security/controller/PostController.java
@@ -1,13 +1,45 @@
 package io.started.security.controller;
 
+import io.started.security.domain.common.dto.ApiResponse;
+import io.started.security.domain.post.dto.PostDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(path = "/post", produces = MediaType.APPLICATION_JSON_VALUE)
 public class PostController {
+    @GetMapping
+    public ResponseEntity<ApiResponse<Page<PostDto>>> get() {
+        return ResponseEntity.ok(
+                ApiResponse.valueOf(null));
+    }
 
+    @GetMapping(path = "/{postId}")
+    public ResponseEntity<ApiResponse<PostDto>> get(
+            @PathVariable("postId") long postId) {
+        return ResponseEntity.ok(
+                ApiResponse.valueOf(null));
+    }
+
+    @PostMapping
+    public ResponseEntity<ApiResponse<Void>> post() {
+        long postId = 0L;
+        return ResponseEntity
+                .created(URI.create(String.format("/post/%d", postId)))
+                .body(ApiResponse.emptyOf(HttpStatus.CREATED));
+    }
+
+    @DeleteMapping(path = "/{postId}")
+    public ResponseEntity<ApiResponse<Void>> delete(
+            @PathVariable("postId") long postId) {
+        return ResponseEntity
+                .noContent().build();
+    }
 }
