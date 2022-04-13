0afe89f2c963875372d8e6795bf6bba8297b08c0
chaehoon <youspend@gmail.com>
2022-04-13 15:51:08 +0900

    feat : Controller 공통 응답 클래스 작성

---
+++ src/main/java/io/started/security/domain/common/dto/ApiResponse.java
@@ -0,0 +1,36 @@
+package io.started.security.domain.common.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ApiResponse<T> {
+    private String message;
+    private T data;
+
+    public static ApiResponse<Void> success() {
+        return of(HttpStatus.OK);
+    }
+
+    public static <T> ApiResponse<T> valueOf(T data) {
+        return of(HttpStatus.OK, data);
+    }
+
+    public static ApiResponse<Void> error(HttpStatus httpStatus) {
+        return of(httpStatus);
+    }
+
+    private static ApiResponse<Void> of(HttpStatus httpStatus) {
+        return new ApiResponse<>(httpStatus.getReasonPhrase(), null);
+    }
+
+    private static <T> ApiResponse<T> of(HttpStatus httpStatus, T data) {
+        return new ApiResponse<>(httpStatus.getReasonPhrase(), data);
+    }
+}
